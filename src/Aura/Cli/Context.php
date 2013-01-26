b14cb37ca29fa738f9a545bc195916700e222e70
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-01-27 02:14:52 +0900

    typo

--- src/Aura/Cli/Context.php
+++ src/Aura/Cli/Context.php
@@ -178,7 +178,7 @@ class Context
             return $this->$var;
         } elseif (array_key_exists($key, $this->$var)) {
             // found the requested key.
-            // need the funny {} becuase $var[$key] will try to find a
+            // need the funny {} because $var[$key] will try to find a
             // property named for that element value, not for $var.
             return $this->{$var}[$key];
         } else {
