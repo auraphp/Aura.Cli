0280b9faceed32a10edad0962007adc777705ef7
Paul M. Jones <pmjones88@gmail.com>
2013-09-16 14:45:20 -0500

    use new standard globals.dist.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,8 +6,8 @@ error_reporting(E_ALL);
 require dirname(__DIR__) . '/autoload.php';
 
 // default globals
-if (is_readable(__DIR__ . '/globals.default.php')) {
-    require __DIR__ . '/globals.default.php';
+if (is_readable(__DIR__ . '/globals.dist.php')) {
+    require __DIR__ . '/globals.dist.php';
 }
 
 // override globals
