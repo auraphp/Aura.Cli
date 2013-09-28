6b98f6e03f7ff73ff1d5ba5d4fd792a4258bd7d8
Paul M. Jones <pmjones88@gmail.com>
2013-09-28 13:17:52 -0500

    add shift()

--- src/Context/Argv.php
+++ src/Context/Argv.php
@@ -12,11 +12,22 @@ namespace Aura\Cli\Context;
 
 /**
  * 
- * A read-only representation of $argv.
+ * A read-only representation of $argv values.
  * 
  * @package Aura.Cli
  * 
  */
 class Argv extends AbstractValues
 {
+    /**
+     * 
+     * Shifts the first element off the data values and returns it.
+     * 
+     * @return mixed
+     * 
+     */
+    public function shift()
+    {
+        return array_shift($this->values);
+    }
 }
