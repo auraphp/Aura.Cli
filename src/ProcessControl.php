2c616b891811561bd901fce629ec061759117972
Paul M. Jones <pmjones88@gmail.com>
2014-01-09 01:24:20 -0600

    streamline some and add docs

--- src/ProcessControl.php
+++ src/ProcessControl.php
@@ -10,9 +10,8 @@
  */
 namespace Aura\Cli;
 
-use Aura\Cli\Exception\ExtensionNotAvailable;
-use Aura\Cli\Exception\FunctionNotAvailable;
-use Aura\Cli\Exception\SignalNotCatchable;
+use Aura\Cli\Exception;
+use InvalidArgumentException;
 
 /**
  * Helper for dealing with pcntl signal functions.
@@ -22,36 +21,30 @@ use Aura\Cli\Exception\SignalNotCatchable;
 class ProcessControl
 {
     /**
-     * List of catchable signals
+     * List of catchable signals.
+     * 
+     * We set them in the constructor, not here, because if pcntl isn't
+     * installed then the constants won't exist.
      *
      * @var array
      */
     protected $catchable_signals = array();
 
     /**
-     * The construct will check for any required function that may not be set. Exception is thrown
-     * if the functions are not available. The catchable signals list is set after this, if it's
-     * done before and pcntl isn't installed we get a load more errors from the constant not
-     * existing.
+     * 
+     * Constructor.
+     * 
      */
     public function __construct()
     {
         if (! extension_loaded('pcntl')) {
-            throw new ExtensionNotAvailable('The pcntl extension is not available.');
+            throw new Exception\ExtensionNotAvailable('pcntl');
         }
 
-        $required_functions = array(
-            'pcntl_signal'
-        );
-
-        foreach ($required_functions as $required_function) {
-            if (! function_exists($required_function)) {
-                throw new FunctionNotAvailable(
-                    sprintf("The required function '%s' does not exist.", $required_function)
-                );
-            }
+        if (! function_exists('pcntl_signal')) {
+            throw new Exception\FunctionNotAvailable('pcntl_signal');
         }
-
+        
         $this->catchable_signals = array(
             SIGHUP => SIGHUP,
             SIGINT => SIGINT,
@@ -74,35 +67,46 @@ class ProcessControl
     }
 
     /**
-     * Check if the signal is catchable, then set the signal to be caught by $callable
+     * Check if the signal is catchable, then set the signal to be caught by $handler.
      *
-     * @param int $signal
-     * @param callable|int $handler
+     * @param int $signal The signal to be caught.
+     * 
+     * @param callable|int $handler The handler to be use for the signal.
+     * 
      * @param bool $restart_syscalls
      *
      * @return bool
+     * 
      * @throws Exception\SignalNotCatchable
-     * @throws \InvalidArgumentException
+     * 
+     * @throws InvalidArgumentException
+     * 
      */
     public function __invoke($signal, $handler, $restart_syscalls = true)
     {
         if (! array_key_exists($signal, $this->catchable_signals)) {
-            throw new SignalNotCatchable(sprintf("The signal '%d' is not catchable.", $signal));
-        };
+            throw new Exception\SignalNotCatchable($signal);
+        }
 
         if (! is_callable($handler) && ! is_int($handler)) {
-            throw new \InvalidArgumentException('handler must be of type callable or int.');
+            throw new InvalidArgumentException('Handler must be of type callable or int.');
         }
 
         return $this->catchSignal($signal, $handler, $restart_syscalls);
     }
 
     /**
+     * 
+     * Catches a signal.
+     * 
      * @param int $signal
+     * 
      * @param callable|int $handler
+     * 
      * @param bool $restart_syscalls
-     *
+     * 
      * @return bool
+     * 
      */
     protected function catchSignal($signal, $handler, $restart_syscalls)
     {
